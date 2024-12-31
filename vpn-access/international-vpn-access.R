be3b592cd1459d20e0644bc33604819d32984c14
ANDREW HARRISON (DHC) <andrewh@dhc.nhs.uk>
2024-12-31 14:38:43 +0000

    general refactor replace xlsx package with openxlsx2

--- vpn-access/international-vpn-access.R
+++ vpn-access/international-vpn-access.R
@@ -25,7 +25,9 @@ pacman::p_load(
   devtools,
   mapview,
   webshot,
-  xlsx
+#  xlsx,
+  openxlsx2,
+  httr
 )
 
 #++++++++++++++++++++++++
@@ -36,19 +38,19 @@ pacman::p_load(
 #contain the title
 # - title : the text to use as title
 # - titleStyle : style object to use for title
-xlsx.addTitle<-function(sheet, rowIndex, title, titleStyle){
-  rows <-createRow(sheet,rowIndex=rowIndex)
-  sheetTitle <-createCell(rows, colIndex=1)
-  setCellValue(sheetTitle[[1,1]], title)
-  setCellStyle(sheetTitle[[1,1]], titleStyle)
-}
-
-xlsx.addTitle2<-function(sheet, rowIndex, title, titleStyle){
-  rows <-createRow(sheet,rowIndex=rowIndex)
-  sheetTitle <-createCell(rows, colIndex=2)
-  setCellValue(sheetTitle[[1,1]], title)
-  setCellStyle(sheetTitle[[1,1]], titleStyle)
-}
+# xlsx.addTitle<-function(sheet, rowIndex, title, titleStyle){
+#   rows <-createRow(sheet,rowIndex=rowIndex)
+#   sheetTitle <-createCell(rows, colIndex=1)
+#   setCellValue(sheetTitle[[1,1]], title)
+#   setCellStyle(sheetTitle[[1,1]], titleStyle)
+# }
+# 
+# xlsx.addTitle2<-function(sheet, rowIndex, title, titleStyle){
+#   rows <-createRow(sheet,rowIndex=rowIndex)
+#   sheetTitle <-createCell(rows, colIndex=2)
+#   setCellValue(sheetTitle[[1,1]], title)
+#   setCellStyle(sheetTitle[[1,1]], titleStyle)
+# }
 
 # SET VARIABLES ----
 
@@ -59,12 +61,12 @@ xlsx.addTitle2<-function(sheet, rowIndex, title, titleStyle){
 current_month <- Sys.Date()
 
 # format as a string for naming files
-filenamesuffix <- format(current_month, "%Y_%m_%B")
+filenamesuffix <- format(current_month, "%Y_%m_%d")
 
 # format as a string for printing
 current_month_print <- format(current_month, "%B %Y")
 
-# IMPORT DATA ----
+# IMPORT VPN DATA ----
 
 # import remote access statistics export files
 # one csv file per always-on VPN node
@@ -78,10 +80,25 @@ df_csv3 <- read.csv(file = "csv3.csv")
 # arguments for rbind should reflect the variables above
 dt_ips <- rbind(df_csv2, df_csv3)
 
+# ACQUIRE IP2LOCATION DATA ----
+
+# set ip2location zipfile path
+# api token is stored as environment variable ip2loc_api
+zip_path <- paste("https://www.ip2location.com/download/?token=", Sys.getenv("ip2loc_api"), "&file=DB9LITECSV", sep = "")
+
+# set download file path and name for downloaded data
+file_name <- "temp/IP2LOCATION-LITE-DB9.ZIP"
+
+# download the zipfile
+httr::GET(zip_path, write_disk(file_name, overwrite = TRUE))
+
+# extract the csv file
+unzip(file_name, files = "IP2LOCATION-LITE-DB9.CSV", overwrite = TRUE, exdir = "./temp/")
+
 # import the ip2location csv you have downloaded from
 #  https://lite.ip2location.com/
 # this csv file needs to be located in your working directory
-dt_ip2location <- read.csv(file = "IP2LOCATION-LITE-DB9.CSV", header = FALSE)
+dt_ip2location <- read.csv(file = "temp/IP2LOCATION-LITE-DB9.CSV", header = FALSE)
 
 # the ip2location csv does not come with column headers so we will add these manually
 names(dt_ip2location) <- c("IP_FROM", "IP_TO", "COUNTRY_CODE", "COUNTRY_NAME", "STATE_NAME", "LOCALITY_NAME", "LATITUDE", "LONGITUDE", "POSTCODE")
@@ -124,6 +141,13 @@ setkey(dt_ips, IP_FROM, IP_TO)
 #  AOVPN log entries
 dtm <- foverlaps(dt_ips, dt_ip2location)
 
+# split the date and time in dtm so they are separate
+dtm <- dtm %>%
+  tidyr::separate_wider_delim(ConnectionStartTime, delim = " ", names = c("ConnectionStartDate", "ConnectionStartTime1"))
+
+# format the date
+dtm$ConnectionStartDate <- as.Date(dtm$ConnectionStartDate, "%d/%m/%Y")
+
 # from dtm, create a new table for all connections where country code does not 
 #  equal GB or "-"
 # GB connections are from within the UK
@@ -131,11 +155,11 @@ dtm <- foverlaps(dt_ips, dt_ip2location)
 # dtm_abroad shows all international AOVPN connections
 dtm_abroad <- dtm %>%
   filter(COUNTRY_CODE != "GB" & COUNTRY_CODE != "-") %>%
-  subset(select = c("COUNTRY_CODE", "COUNTRY_NAME", "LOCALITY_NAME", "Username", "ConnectionStartTime", "ClientIPAddress", "ClientExternalAddress", "LATITUDE", "LONGITUDE")) %>%
+  subset(select = c("COUNTRY_CODE", "COUNTRY_NAME", "LOCALITY_NAME", "Username", "ConnectionStartDate", "ConnectionStartTime1", "ClientIPAddress", "ClientExternalAddress", "LATITUDE", "LONGITUDE")) %>%
   arrange(COUNTRY_CODE)
 
 dtm_abroad_person <- dtm_abroad %>%
-  subset(select = c("Username", "COUNTRY_CODE", "COUNTRY_NAME", "LOCALITY_NAME", "ConnectionStartTime", "ClientIPAddress", "ClientExternalAddress", "LATITUDE", "LONGITUDE")) %>%
+  subset(select = c("Username", "COUNTRY_CODE", "COUNTRY_NAME", "LOCALITY_NAME", "ConnectionStartDate", "ConnectionStartTime1", "ClientIPAddress", "ClientExternalAddress", "LATITUDE", "LONGITUDE")) %>%
   arrange(Username)
 
 # from dtm, create a new table for all connections within the UK
@@ -148,23 +172,25 @@ dtm_group <- dtm_abroad %>%
   subset(select = c("COUNTRY_NAME", "LOCALITY_NAME", "Username")) %>%
   arrange(COUNTRY_NAME)
 
-# PRESENT AND EXPORT DATA ----
+# PRESENT AND EXPORT DATA AS CSV ----
 
 # export to csv - all
-write.csv(dtm, file = paste("vpn_all_", filenamesuffix, ".csv" , sep = ""), row.names = FALSE)
+# useful for testing but not needed in live
+# write.csv(dtm, file = paste("vpn_all_", filenamesuffix, ".csv" , sep = ""), row.names = FALSE)
 
 # export to csv - international only
-write.csv(dtm_abroad, file = paste("vpn_intl_", filenamesuffix, ".csv" , sep = ""), row.names = FALSE)
+# useful for testing but not needed in live
+# write.csv(dtm_abroad, file = paste("vpn_intl_", filenamesuffix, ".csv" , sep = ""), row.names = FALSE)
 
 # export to csv - GB only
-write.csv(dtm_gb, file = paste("vpn_gb_", filenamesuffix, ".csv" , sep = ""), row.names = FALSE)
+# write.csv(dtm_gb, file = paste("vpn_gb_", filenamesuffix, ".csv" , sep = ""), row.names = FALSE)
 
 # export international to xlsx
 # xlsx::write.xlsx(dtm_abroad, file = paste("vpn_intl_", filenamesuffix, ".xlsx" , sep = ""), sheetName = "By Country", append = FALSE, row.names = FALSE)
 # xlsx::write.xlsx(dtm_abroad_person, file = paste("vpn_intl_", filenamesuffix, ".xlsx" , sep = ""), sheetName = "By Person", append = TRUE, row.names = FALSE)
 
-# DO NOT SELECT BELOW THIS LINE UNLESS YOU WANT TO CREATE MAPS ----
-# PROCESSOR INTENSIVE
+# MAPS ----
+# CAN BE PROCESSOR INTENSIVE
 
 # INTERNATIONAL CONNECTIONS MAP ----
 # create a map for international connections
@@ -176,61 +202,80 @@ map_abroad
 webshot::install_phantomjs()
 mapview::mapshot(map_abroad, file = paste("vpn_intl_map_", filenamesuffix, ".png" , sep = ""))
 
+# create a var with the file name
+img_map <- paste("vpn_intl_map_", filenamesuffix, ".png" , sep = "")
+
 # GB CONNECTIONS MAP ----
 # create a map for GB connections - takes a while
-map_gb <- mapview::mapview(dtm_gb, xcol = "LONGITUDE", ycol = "LATITUDE", crs = 4326, grid = FALSE)
+# and is not all that reliable
+# map_gb <- mapview::mapview(dtm_gb, xcol = "LONGITUDE", ycol = "LATITUDE", crs = 4326, grid = FALSE)
 
 # draw map - takes a while
-map_gb
+# map_gb
 
 # EXCEL FILE ----
 
-intl_workbook <- xlsx::createWorkbook(type = "xlsx")
-
-# Title and sub title styles
-TITLE_STYLE <- CellStyle(intl_workbook)+ Font(intl_workbook,  heightInPoints=16, 
-                                   color="blue", isBold=TRUE, underline=0)
-
-SUB_TITLE_STYLE <- CellStyle(intl_workbook) + 
-  Font(intl_workbook,  heightInPoints=12, 
-       isItalic=FALSE, isBold=FALSE)
-
-# Styles for the data table row/column names
-TABLE_ROWNAMES_STYLE <- CellStyle(intl_workbook) + Font(intl_workbook, isBold=TRUE)
-TABLE_COLNAMES_STYLE <- CellStyle(intl_workbook) + Font(intl_workbook, isBold=TRUE) +
-  Alignment(wrapText=TRUE, horizontal="ALIGN_CENTER") +
-  Border(color="black", position=c("TOP", "BOTTOM"), 
-         pen=c("BORDER_THIN", "BORDER_THICK")) 
-
-sheet_front <- xlsx::createSheet(intl_workbook, sheetName = "Cover")
-xlsx::addPicture("DHC_logo_small.png", sheet_front, scale = 1, startRow = 2, startColumn = 2)
-# Add title
-xlsx.addTitle2(sheet_front, rowIndex = 10, title = "International remote worker access report", titleStyle = TITLE_STYLE)
-xlsx.addTitle2(sheet_front, rowIndex = 11, title = paste(current_month_print), titleStyle = TITLE_STYLE)
-xlsx.addTitle2(sheet_front, rowIndex = 14, title = paste0("Automated report plotted at", Sys.time(), sep = " "), titleStyle = SUB_TITLE_STYLE)
-xlsx.addTitle2(sheet_front, rowIndex = 14, title = "Contact point: andrew.harrison11@nhs.net", titleStyle = SUB_TITLE_STYLE)
+# create the start and end dates for the title
+date_start <- min(dtm_abroad$ConnectionStartDate)
+date_end <- max(dtm_abroad$ConnectionStartDate)
+date_start_print <- format(date_start, "%d %B %Y")
+date_end_print <- format(date_end, "%d %B %Y")
+date_string <- paste(date_start_print, "to", date_end_print, sep = " ")
+date_report <- paste("Report generated on", format(Sys.Date(), "%d %B %Y"), sep = " ")
 
 
 
-sheet_by_country <- xlsx::createSheet(intl_workbook, sheetName = "By Country")
-xlsx::addDataFrame(dtm_abroad, sheet_by_country, startRow = 3, startColumn = 1, colnamesStyle = TABLE_COLNAMES_STYLE, rownamesStyle = TABLE_ROWNAMES_STYLE, row.names = FALSE)
-xlsx::setColumnWidth(sheet_by_country, colIndex = c(1:ncol(dtm_abroad)), colWidth = 30)
-# Add title
-xlsx.addTitle(sheet_by_country, rowIndex=1, title="Connections by country",
-              titleStyle = TITLE_STYLE)
+# Using openxlsx2, initialise a workbook
+#  and add worksheets
+wb <- openxlsx2::wb_workbook()
+wb$add_worksheet("Cover")
+wb$add_worksheet("By country")
+wb$add_worksheet("By name")
+wb$add_worksheet("Map")
 
-sheet_by_name <- xlsx::createSheet(intl_workbook, sheetName = "By Name")
-xlsx::addDataFrame(dtm_abroad_person, sheet_by_name, startRow = 3, startColumn = 1, colnamesStyle = TABLE_COLNAMES_STYLE, rownamesStyle = TABLE_ROWNAMES_STYLE, row.names = FALSE)
-xlsx::setColumnWidth(sheet_by_name, colIndex = c(1:ncol(dtm_abroad)), colWidth = 30)
-# Add title
-xlsx.addTitle(sheet_by_name, rowIndex=1, title="Connections by name",
-              titleStyle = TITLE_STYLE)
+# Create the cover worksheet
+# data
+wb$add_image("Cover", file = "DHC_logo_small.png", dims = "B2", width = 5.93, height = 3.32, units = "cm")
+wb$add_data("Cover", "International traveller access report", start_col = 2, start_row = 9)
+wb$add_data("Cover", date_string, start_col = 2, start_row = 10)
+wb$add_data("Cover", date_report, start_col = 2, start_row = 11)
+wb$add_data("Cover", "Contact point: andrew.harrison11@nhs.net", start_col = 2, start_row = 13)
+# formatting
+wb$add_font("Cover", dims = "B9:B10", name = "Arial", color = wb_colour(hex = "900C3F"), size = 14, bold = "double")
+wb$add_font("Cover", dims = "B11:B13", name = "Arial", color = wb_colour(hex = "900C3F"), size = 11, bold = "")
+wb$add_fill("Cover", dims = "A1:J25", color = wb_colour(hex = "DBFFF9"), pattern = "solid")
 
-sheet_map <- xlsx::createSheet(intl_workbook, sheetName = "Map")
-xlsx::addPicture(paste("vpn_intl_map_", filenamesuffix, ".png" , sep = ""), sheet_map, scale = 1, startRow = 4, startColumn = 1)
-# Add title
-xlsx.addTitle(sheet_map, rowIndex=1, title="Connections map",
-              titleStyle = TITLE_STYLE)
+# Create the by country worksheet
+# data
+wb$add_data("By country", "Connections by country", start_col = 1, start_row = 1)
+wb$add_data_table("By country", dtm_abroad, start_col = 1, start_row = 3, col_names = TRUE, row_names = FALSE, with_filter = TRUE, total_row = FALSE)
+# formatting
+wb$set_col_widths("By country", cols = 1:10, widths = "auto")
+wb$add_font("By country", dims = "A1", name = "Arial", color = wb_colour(hex = "900C3F"), size = 14, bold = "double")
+wb$add_font("By country", dims = "A3:J3", bold = "single", color = wb_colour(hex = "FFFFFF"))
+wb$add_fill("By country", dims = "A1:J2", color = wb_colour(hex = "DBFFF9"), pattern = "solid")
 
-# save
-xlsx::saveWorkbook(intl_workbook, paste("vpn_intl_", filenamesuffix, ".xlsx" , sep = ""))
+# Create the by name worksheet
+# data
+wb$add_data("By name", "Connections by name", start_col = 1, start_row = 1)
+wb$add_data_table("By name", dtm_abroad_person, start_col = 1, start_row = 3, col_names = TRUE, row_names = FALSE, with_filter = TRUE, total_row = FALSE)
+# formatting
+wb$set_col_widths("By name", cols = 1:10, widths = "auto")
+wb$add_font("By name", dims = "A1", name = "Arial", color = wb_colour(hex = "900C3F"), size = 14, bold = "double")
+wb$add_font("By name", dims = "A3:J3", bold = "single", color = wb_colour(hex = "FFFFFF"))
+wb$add_fill("By name", dims = "A1:J2", color = wb_colour(hex = "DBFFF9"), pattern = "solid")
+
+# Create the map worksheet
+# data
+wb$add_data("Map", "Map of connections", start_col = 1, start_row = 1)
+wb$add_image("Map", file = img_map, dims = "A3", width = 26.24, height = 20.11, units = "cm")
+# formatting
+wb$add_font("Map", dims = "A1", name = "Arial", color = wb_colour(hex = "900C3F"), size = 14, bold = "double")
+wb$add_fill("Map", dims = "A1:P42", color = wb_colour(hex = "DBFFF9"), pattern = "solid")
+
+# opens the workbook in Excel
+# needed for testing only
+# wb$open()
+
+# save the workbook
+wb$save(file = paste("International_travellers_", filenamesuffix, ".xlsx" , sep = ""))
